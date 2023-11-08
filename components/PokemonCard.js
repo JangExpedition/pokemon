9591564193b8d7e7c937b679edafb0b7f772db1b
Jangcccci <jangcccci@gmail.com>
2023-11-08 22:55:30 +0900

    lazy 이미지 생성

--- components/PokemonCard.js
+++ components/PokemonCard.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 
 const PokemonCard = ({ pokemon }) => {
   const [pokemonData, setPokemonData] = useState({});
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     getPokemonData(pokemon);
@@ -11,24 +12,41 @@ const PokemonCard = ({ pokemon }) => {
   const getPokemonData = async (pokemon) => {
     try {
       const response = await axios.get(pokemon.url);
-      console.log(response.data.sprites.other["official-artwork"].front_default);
       setPokemonData({
         id: response.data.id,
         image: response.data.sprites.other["official-artwork"].front_default,
         name: response.data.name,
+        type: response.data.types[0].type.name,
       });
     } catch (error) {
       console.log(error);
     }
   };
 
+  const imageHandler = (target) => {
+    target.style.display = "inline-block";
+    setLoading(false);
+  };
+
   return (
     <div className="PokemonCard">
       <p>#{String(pokemonData?.id).padStart(3, "0")}</p>
       <div className="pokemon-image-wrapper">
-        <img src={pokemonData?.image} />
+        {loading && (
+          <div className="loading">
+            <span>...loading</span>
+          </div>
+        )}
+        <img
+          src={pokemonData?.image}
+          alt={pokemonData?.name}
+          loading="lazy"
+          onLoad={(e) => {
+            imageHandler(e.target);
+          }}
+        />
       </div>
-      <div className="pokemon-name-wrapper">
+      <div className={`pokemon-name-wrapper ${pokemonData?.type}`}>
         <span className="pokemon-name">{pokemonData.name?.toUpperCase()}</span>
       </div>
     </div>
